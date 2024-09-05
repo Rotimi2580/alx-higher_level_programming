803482b2ae75c6d4f0073c0e75eee0da81dff2e3
Rotimi2580 <Rotimi2580@github.com>
2024-09-05 03:47:06 -0700

    How create new user with password andgrant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates user in a database with a password and granted all priviledges.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
