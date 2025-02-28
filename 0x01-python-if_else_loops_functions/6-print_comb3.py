d7f9583eb4a792aaec407d6f4e39b58b835085aa
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-02-28 09:22:51 +0000

    review of double digit code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(0, 10, 1):
     for j in range(i+1, 10, 1):
         if i != 8:
-        print("{}{}".format(i, j), end=', ')
+            print("{}{}".format(i, j), end=', ')
 print('89')
