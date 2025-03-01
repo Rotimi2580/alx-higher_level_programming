1cfdf4ba1b768254a92338d2ab935e1500046539
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-01 08:27:52 +0000

    futher review of uppercase function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,7 +6,7 @@ def uppercase(str):
         if c in range(65, 91, 1):
             new_str += chr(c)
         elif c in range(97, 123, 1):
-            new_str += chr(c -32)
+            new_str += chr(c - 32)
         else:
             new_str += chr(c)
     print("{}".format(new_str))
