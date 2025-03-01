a84c6f305078b1944ee7e324b8ac7beec0f336c2
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-01 09:04:39 +0000

    code review

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number > 0:
         return (number % 10)
     elif number < 0:
-        return (number % -10)
+        return -(number % 10)
     else:
         return (0)
