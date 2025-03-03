632e41ea1c80ae8242bb002795d4291d6902d0e1
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-03 10:02:29 +0000

    reviewed fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101, 1):
+    for i in range(1, 101):
         if (i % 3) and (i % 5) == 0:
             print("{}".format("FizzBuzz"), end=' ')
         elif (i % 3) == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif (i % 5) == 0:
             print("{}".format("Buzz"), end=' ')
         else:
-            print("{}".format(i), end=' ') 
+            print("{}".format(i), end=' ')
