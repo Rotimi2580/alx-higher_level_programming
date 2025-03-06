f677d25d030d1100c0442dbcba59a9f36d506a10
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-06 12:56:23 +0000

    review and further review of code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx not in my_list or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return ("None")
     else:
         return (my_list[idx])
