8c92335cd31baefbb79ff976e01f3a6d779031b3
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-07 08:49:15 +0000

    a program that removes C or c from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return
+    else:
+        new_str = ""
+        for i in my_string:
+            if i == 'c' or i == 'C':
+                continue
+            else:
+                new_str += i
+        return (new'str)
