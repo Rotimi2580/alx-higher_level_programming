d846d08304aee7e80c29306b48b0dc9ccacb4b95
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-20 07:30:42 +0000

    code to print squares of elements in a 2D matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+matrix:
+    if not matrix:
+        return []
+    else:
+        return [list(map(lambda x : x**2, i))]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This repository is composed of files on more tasks on Data structure in python
