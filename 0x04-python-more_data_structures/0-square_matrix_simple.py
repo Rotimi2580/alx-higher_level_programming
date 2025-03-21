974d36a2580f3373b650dd6ff79d7c34df72d061
Rotimi2580 <rotimi.omitoogun@gmail.com>
2025-03-21 15:38:49 +0000

    further review of code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ matrix:
     if not matrix:
         return []
     else:
-        return [list(map(lambda x : x**2, i) for i in matrix)]
+        return [list(map(lambda x : x**2, i)) for i in matrix]
